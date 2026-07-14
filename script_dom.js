3032675d0dcb0dfd138cf4510f9f80c7b55f222d
Pedroccezar <pedro.h.c.castilho@gmail.com>
2026-07-14 19:39:21 -0300

    feat: add itens

--- script_dom.js
+++ script_dom.js
@@ -3,6 +3,11 @@ const exit_botton = document.getElementById("botaosaida")
 const add_overlay = document.getElementById("overlay")
 const cancel_botton = document.getElementById("botaocancelar")
 const empty_list = document.getElementById("listavazia")
+const add_item_overley_botton = document.getElementById("botaoadditemoverlay")
+const lista_preenchida = document.getElementById("filled_list")
+const name_product_input = document.getElementById("nome")
+const qnt_product_input = document.getElementById("quantidade")
+const filled_list = document.getElementById("filled_list")
 
 add_botton.addEventListener("click", () => {
     add_overlay.showModal();
@@ -13,5 +18,27 @@ exit_botton.addEventListener("click", () => {
 })
 
 cancel_botton.addEventListener("click", () => {
+    add_overlay.close();
+})
+
+add_item_overley_botton.addEventListener("click", () => {
+    empty_list.classList.add("hidden");
+    const nome = name_product_input.value
+    const qnt = qnt_product_input.value
+
+    const item = document.createElement("li")
+    item.classList.add("itemcard")
+    item.innerHTML = `
+    <span class="item_bullet"></span>
+    <span class="item_nome">${nome}</span>
+    ${qnt ? `<span class="item_qnt">${qnt}</span>` : ""}
+`
+
+    filled_list.appendChild(item)
+    filled_list.classList.remove("hidden")
+
+    name_product_input.value = ""
+    qnt_product_input.value = ""
+
     add_overlay.close();
 })
